333ec8d23abca0ce499ffa9e5d76ecda59814675
erib <eri-b@users.noreply.github.com>
2019-06-03 10:58:35 -0400

    more tests

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -3,47 +3,52 @@ require_relative '../lib/game'
 require_relative '../lib/player'
 require_relative '../lib/ui'
 
-# require_relative '../bin/game'
 
-# RSpec.describe 'Board' do
-
-#   describe "#game_over" do
-
-  #   it "returns true if draw (full board)" do
-  #     @board = {}
-  #     @score = {}
-  #     expect(game_over).to eql(true)
-  #   end
-  #
-  #   it "returns true if winner and full board" do
-  #     @board = []
-  #     @score =[]
-  #     expect(game_over).to eql(true)
-  #   end
-#     it "returns true if winner and partial board" do
-#       @board = {"11":"X", "12":"X", "13":"X"}
-#       @score = {"row 1": "XXX"}
-#       expect(Board.game_over).to eql(true)
-#     end
-
-#   end
-
-
-# end
 
 RSpec.describe Board do
   describe "#score" do
     let(:new_board) {Board.new}
-    let(:player1) {Player.new('a', "X", new_board)}
-    let(:player2) {Player.new('b', "O", new_board)}
+    let(:player1) {Player.new('Eric', "X", new_board)}
+    let(:player2) {Player.new('Marcos', "O", new_board)}
 
     let(:game) {Game.new(player1, player2, new_board)}
 
-    it "test 1" do
-      
+    it "O wins" do
+      new_board.board = {"11" => "O", "12" => "O", "13" => "O"}
       new_board.score["row 1"] = "OOO"
 
       expect(game.game_on).to eql(false)
     end
+
+    it "X wins" do
+      new_board.board = {"31" => "X", "32" => "X", "33" => "X"}
+      new_board.score["row 3"] = "XXX"
+
+      expect(game.game_on).to eql(false)
+    end
+
+    it "Game not over" do
+      new_board.board = {"31" => "X", "32" => "O", "33" => "X"}
+      new_board.score["row 3"] = "XOX"
+
+      expect(game.game_on).to be_truthy
+    end
+
+    it "Game is draw" do
+      new_board.board = {"11" => "X", "12" => "O", "13" => "X", "21" => "O", "22" => "X", "23" => "O", "31" => "O", "32" => "X", "33" => "O"}
+      new_board.score["row 1"] = "XOX"
+      new_board.score["row 2"] = "OXO"
+      new_board.score["row 3"] = "OXO"
+      new_board.score["col 1"] = "XOO"
+      new_board.score["col 2"] = "OXX"
+      new_board.score["col 3"] = "XOO"
+      new_board.score["diag1 1"] = "XXO"
+      new_board.score["diag 2"] = "XXO"
+
+
+      expect(game.game_on).to be_truthy
+    end
+
+
   end
 end
