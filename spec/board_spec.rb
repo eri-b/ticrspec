0a97d9f2ab95a7bff8da45a6cc17ba6dadbf4cfb
erib <eri-b@users.noreply.github.com>
2019-06-03 16:14:12 -0400

    all 8 cases

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -13,17 +13,51 @@ RSpec.describe Board do
     let(:player2) {Player.new('Marcos', "O", new_board)}
     let(:game) {Game.new(player1, player2, new_board)}
 
-    it "Player O wins" do
+    it "Winning Board 1" do
       new_board.values = {"11" => "O", "12" => "O", "13" => "O"}
       new_board.score["row 1"] = "OOO"
-
       expect(game.game_on).to eql(false)
     end
 
-    it "Player X wins" do
+    it "Winning Board 2" do
+      new_board.values = {"21" => "X", "22" => "X", "23" => "X"}
+      new_board.score["row 2"] = "XXX"
+      expect(game.game_on).to eql(false)
+    end
+
+    it "Winning Board 3" do
       new_board.values = {"31" => "X", "32" => "X", "33" => "X"}
       new_board.score["row 3"] = "XXX"
+      expect(game.game_on).to eql(false)
+    end
 
+    it "Winning Board 4" do
+      new_board.values = {"11" => "X", "21" => "X", "31" => "X"}
+      new_board.score["col 1"] = "XXX"
+      expect(game.game_on).to eql(false)
+    end
+
+    it "Winning Board 5" do
+      new_board.values = {"12" => "X", "22" => "X", "32" => "X"}
+      new_board.score["col 2"] = "XXX"
+      expect(game.game_on).to eql(false)
+    end
+
+    it "Winning Board 6" do
+      new_board.values = {"13" => "X", "23" => "X", "33" => "X"}
+      new_board.score["col 3"] = "XXX"
+      expect(game.game_on).to eql(false)
+    end
+
+    it "Winning Board 7" do
+      new_board.values = {"11" => "X", "22" => "X", "33" => "X"}
+      new_board.score["diag 1"] = "XXX"
+      expect(game.game_on).to eql(false)
+    end
+
+    it "Winning Board 8" do
+      new_board.values = {"13" => "X", "22" => "X", "31" => "X"}
+      new_board.score["diag 2"] = "XXX"
       expect(game.game_on).to eql(false)
     end
 
